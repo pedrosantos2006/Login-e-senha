fa92ff99b68ae235776183b0cacf5712a5bec078
Pedro Santos <112526196+pedrosantos2006@users.noreply.github.com>
2023-03-29 20:12:28 -0300

    Adicionar arquivos de projeto.

---
+++ Login e senha/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Login_e_senha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxLogin = new System.Windows.Forms.TextBox();
+            this.TextBoxSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ButtonLogin = new System.Windows.Forms.Button();
+            this.ButtonCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(66, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(66, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LOGIN";
+            // 
+            // TextBoxLogin
+            // 
+            this.TextBoxLogin.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxLogin.Location = new System.Drawing.Point(138, 110);
+            this.TextBoxLogin.Name = "TextBoxLogin";
+            this.TextBoxLogin.Size = new System.Drawing.Size(179, 26);
+            this.TextBoxLogin.TabIndex = 2;
+            // 
+            // TextBoxSenha
+            // 
+            this.TextBoxSenha.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxSenha.Location = new System.Drawing.Point(138, 181);
+            this.TextBoxSenha.Name = "TextBoxSenha";
+            this.TextBoxSenha.Size = new System.Drawing.Size(179, 26);
+            this.TextBoxSenha.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(66, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SENHA";
+            // 
+            // ButtonLogin
+            // 
+            this.ButtonLogin.BackColor = System.Drawing.Color.LightGray;
+            this.ButtonLogin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.ButtonLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonLogin.Location = new System.Drawing.Point(138, 255);
+            this.ButtonLogin.Name = "ButtonLogin";
+            this.ButtonLogin.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLogin.TabIndex = 5;
+            this.ButtonLogin.Text = "LOGIN";
+            this.ButtonLogin.UseVisualStyleBackColor = false;
+            this.ButtonLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
+            // ButtonCancelar
+            // 
+            this.ButtonCancelar.BackColor = System.Drawing.Color.LightGray;
+            this.ButtonCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.ButtonCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonCancelar.Location = new System.Drawing.Point(242, 255);
+            this.ButtonCancelar.Name = "ButtonCancelar";
+            this.ButtonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancelar.TabIndex = 6;
+            this.ButtonCancelar.Text = "CANCELAR";
+            this.ButtonCancelar.UseVisualStyleBackColor = false;
+            this.ButtonCancelar.Click += new System.EventHandler(this.ButtonCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonCancelar);
+            this.Controls.Add(this.ButtonLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBoxSenha);
+            this.Controls.Add(this.TextBoxLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxLogin;
+        private System.Windows.Forms.TextBox TextBoxSenha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ButtonLogin;
+        private System.Windows.Forms.Button ButtonCancelar;
+    }
+}
+
---
+++ Login e senha/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Login_e_senha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            if (TextBoxLogin.Text == "Pedro" && TextBoxSenha.Text == "pedro@14")
+            {
+                MessageBox.Show("Bem Vindo ao sistema!", "Acesso ao Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Acesso Negado!", "Acesso Negado !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
